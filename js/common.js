57a7525bf5eef398e325f5fc27a61207118d6fcd
unknown <testingtest0843@gmail.com>
2020-10-26 11:29:47 +0900

    slide,header add

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(function(){
+//start    
+    $('header').load('common.html header>div'),
+    $('footer').load('common.html footer div'),
+    $('aside').load('common.html aside ul');
+    
+//end    
+})
